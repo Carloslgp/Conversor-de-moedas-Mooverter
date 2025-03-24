8ee1f2d8e8fecb4fc3758d9e26ba58bd2a457366
Carloslgp <carloslgp3585@gmail.com>
2025-03-24 18:09:31 -0300

    Adicionando resultados e alterando estilos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 function converter() {
     const audio = new Audio('assets/sound/moosound.mp3')
     audio.play()
+    let paragrafoResultado = document.querySelector("#resultado_texto")
     let moedaBase = document.querySelector("#select_base").value
     let converterParaMoeda = document.querySelector("#select_converter_para").value
     let valorBase = document.querySelector("#inputValorBase").value
     fetch(`https://api.frankfurter.dev/v1/latest?base=${moedaBase}&symbols=${converterParaMoeda}`)
     .then((resp) => resp.json())
     .then((data) => {
-        const quantiaConvertida = (valorBase * data.rates[converterParaMoeda]).toFixed(2);
-        console.log(`${valorBase} ${moedaBase} = ${quantiaConvertida} ${converterParaMoeda}`);
+        const quantiaConvertida = (valorBase * data.rates[converterParaMoeda]).toFixed(2)
+        paragrafoResultado.textContent = quantiaConvertida;
+        document.querySelector(".paragrafo_resultado_moeda").textContent = converterParaMoeda;
     });
+
 }
 
 
